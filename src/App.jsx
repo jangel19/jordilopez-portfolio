3133940813494417627c99c4f7695a412c645a10
jangel19 <lopesjordi0@gmail.com>
2025-07-23 19:46:19 -0400

    forgot to delete the old function

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import LandingSection from './components/LandingSection'
 import AboutSection from './components/AboutSection'
 import ProjectsSection from './components/ProjectsSection'
 import ContactSection from './components/ContactSection'
-import logVisitor from './components/logVisitor'
-import { useEffect } from 'react'
 
 function App() {
-  // log visitor data on page load
-  useEffect(() => {
-    logVisitor();
-  }, []);
+
   // main app component
   return (
     <div className="App">
