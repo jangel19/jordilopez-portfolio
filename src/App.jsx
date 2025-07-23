4f096e779e54dd33b63c31f34c7c455b3b76b61d
jangel19 <lopesjordi0@gmail.com>
2025-07-23 14:59:08 -0400

    updated the website and included visitor log using supabase and JS

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import LandingSection from './components/LandingSection'
 import AboutSection from './components/AboutSection'
 import ProjectsSection from './components/ProjectsSection'
 import ContactSection from './components/ContactSection'
+import logVisitor from './components/logVisitor'
+import { useEffect } from 'react'
 
 function App() {
+  // log visitor data on page load
+  useEffect(() => {
+    logVisitor();
+  }, []);
+  // main app component
   return (
     <div className="App">
       <LandingSection />
@@ -11,7 +18,7 @@ function App() {
       <ProjectsSection />
       <ContactSection />
     </div>
-  )
+  );
 }
 
 export default App
